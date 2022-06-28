ad1d3d7bcb83747ca068c3c5f0dfa0484ce10d5d
Inkve <levin.andrei.2003@gmail.com>
2022-06-28 16:00:07 +0300

    Добавьте файлы проекта.

---
+++ Practice_1_course/Practice_1_course.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int GetRandomNumber(int, int);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int m, n;
+    std::vector <int> data_array;
+    std::cout << "Введите размер первой части массива, m = ";
+    std::cin >> m;
+    std::cout << "Введите размер второй части массива, n = ";
+    std::cin >> n;
+    
+    for (int i = 0; i < m + n; i++) {
+        data_array.push_back(GetRandomNumber(-10, 10));
+    }
+}
+
+int GetRandomNumber(int min, int max) {
+    srand(time(NULL));
+    return min + rand() % (max - min + 1);
+}; 
